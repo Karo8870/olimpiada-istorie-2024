62a66a61d25ec9156c2315e5d23e94e61ba991f7
alex <simedrea.alex@gmail.com>
2024-03-21 20:26:24 +0200

    responsiveness WIP

--- src/components/large-button.tsx
+++ src/components/large-button.tsx
@@ -1,18 +1,31 @@
 import '@/lib/fontawesome/css/fa.css';
+import { cn } from '@/lib/utils';
 
 export default function LargeButton({
   text,
-  className
+  className,
+  arrowClassName
 }: {
   text: string;
-  className: string;
+  className?: string;
+  arrowClassName?: string;
 }) {
   return (
     <button
-      className={`flex h-full min-h-16 w-full items-center justify-center rounded-[30px] border-3 border-black px-16 py-4 text-left text-lg font-semibold leading-tight ${className}`}
+      className={cn(
+        'flex h-full min-h-16 w-full items-center justify-between rounded-[30px] border-3 border-black max-xl:px-4 px-8 py-4 text-left text-lg font-semibold leading-tight max-xl:text-base',
+        className
+      )}
     >
       {text}
-      <i className='fa fa-arrow-down ml-2'></i>
+      <div
+        className={cn(
+          'ml-2 flex h-12 w-12 min-w-12 max-w-12 items-center justify-center rounded-full bg-black max-xl:h-8 max-xl:w-8 max-xl:min-w-8 max-xl:max-w-8',
+          arrowClassName
+        )}
+      >
+        <i className='fa fa-arrow-down max-xl:text-base text-xl'></i>
+      </div>
     </button>
   );
 }
