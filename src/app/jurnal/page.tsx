ba02d162744e58197e0aa61d4dd704b4bd022772
alex <simedrea.alex@gmail.com>
2024-04-04 12:44:38 +0300

    made changes 2

--- src/app/jurnal/page.tsx
+++ src/app/jurnal/page.tsx
@@ -1,5 +1,20 @@
 function Jurnal() {
-  return <></>;
+  return (
+    <main className='flex flex-col gap-8 px-16 pb-16 pt-32 sm:px-4'>
+      <div className='relative flex w-full flex-col items-center bg-cover'>
+        <img
+          className='absolute z-[0] h-full w-full rounded-[1.25rem] object-cover [filter:brightness(65%)]'
+          src='/brasov4.jpeg'
+          alt='Brasov'
+        />
+        <div className='mb-48 mt-48 flex w-[80%] max-w-[60rem] flex-col items-center'>
+          <h1 className='relative z-10 text-[3rem] font-bold text-white md:text-3xl'>
+            Jurnal
+          </h1>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Jurnal;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,109 +3,110 @@
 import useNav from '@/components/useNav';
 import { useState } from 'react';
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function AppNavbar() {
-  const navMode = usePathname() === '/' ? useNav() : false;
+  const x = useNav();
+  const navMode = usePathname() === '/' ? x : false;
 
   const [open, setOpen] = useState<boolean>(false);
 
   return (
     <>
       <nav
-
         className={`fixed left-0 top-0 z-50 flex w-full gap-[1vh] bg-white md:hidden ${navMode ? 'flex-col pt-[2vh]' : 'flex-row justify-center p-[1vh]'}`}
       >
         <div
-
           className={`flex grow basis-0 items-center ${navMode ? 'flex-col gap-1' : 'flex-row gap-6'}`}
         >
           <div>
-            <img
-              className={`h-12 w-12 min-w-12 max-w-12 flex-grow basis-0 justify-end ${navMode ? 'hidden' : 'flex'}`}
-              src='logo.png'
-              alt='icon'
-            />
+            <Link href='/'>
+              <img
+                className={`h-12 w-12 min-w-12 max-w-12 flex-grow basis-0 justify-end ${navMode ? 'hidden' : 'flex'}`}
+                src='/logo.png'
+                alt='icon'
+              />
+            </Link>
             <div
-              className={`flex-grow basis-0 justify-start ${navMode ? 'flex' : 'hidden'}`}
+              className={`flex-grow basis-0 justify-start gap-10 ${navMode ? 'flex' : 'hidden'}`}
             >
-              <img className='h-[8vh]' src='logo.png' alt='icon' />
-              <img className='h-[8vh]' src='logo.png' alt='icon' />
-              <img className='h-[8vh]' src='logo.png' alt='icon' />
-              <img className='h-[8vh]' src='logo.png' alt='icon' />
-              <img className='h-[8vh]' src='logo.png' alt='icon' />
-              <img className='h-[8vh]' src='logo.png' alt='icon' />
+              <img className='h-[8vh]' src='/logos/isj.png' alt='icon' />
+              <img className='h-[8vh]' src='/logos/me.png' alt='icon' />
+              <img className='h-[8vh]' src='/logos/primaria.png' alt='icon' />
+              <img className='h-[8vh]' src='/logos/consiliu.png' alt='icon' />
+              <img className='h-[8vh]' src='/logos/saguna.jpg' alt='icon' />
             </div>
           </div>
-          <a
-            href='/'
-            className={`flex flex-col items-center`}
-          >
+          <Link href='/' className={`flex flex-col items-center cursor-pointer`}>
             <label
-              className={`text-center font-bold ${navMode ? 'text-[0px] text-white' : 'text-[1rem] text-black'}`}
+              className={`cursor-pointer text-center font-bold ${navMode ? 'text-[0px] text-white' : 'text-[1rem] text-black'}`}
             >
               OLIMPIADA DE ISTORIE - ETAPA NATIONALA
             </label>
             <label
-              className={`text-center font-bold ${navMode ? 'text-[0px] text-white' : 'text-[1rem] text-black'}`}
+              className={`cursor-pointer text-center font-bold ${navMode ? 'text-[0px] text-white' : 'text-[1rem] text-black'}`}
             >
               EDITIA XLVII, 2024
             </label>
-          </a>
+          </Link>
         </div>
         <div className={`flex items-center justify-center gap-8`}>
-          <a
-            href='/concurs'
-            className='text-base font-bold'
-          >
+          <a href='/concurs' className='text-base font-bold'>
             Concurs
           </a>
-          <a
-            href='/program'
-            className='text-base font-bold'
-          >
+          <a href='/program' className='text-base font-bold'>
             Program
           </a>
-          <a
-            href='/informatii'
-            className='text-base font-bold'
-          >
-            Informații
+          <a href='/informatii' className='text-base font-bold'>
+            Informații utile
           </a>
-          <a
-            href='/jurnal'
-            className='text-base font-bold'
-          >
-            Jurnal
+          <a href='/jurnal' className='text-base font-bold'>
+            Jurnalul olimpiadei
           </a>
-          <a
-            href='/contact'
-            className='text-base font-bold'
-          >
-            Contact
+          <a href='/' className='text-base font-bold'>
+            Parteneri și sponsori
           </a>
         </div>
-        <div
-          className={`grow basis-0 ${navMode ? 'hidden' : ''}`}
-        ></div>
+        <div className={`grow basis-0 ${navMode ? 'hidden' : ''}`}></div>
       </nav>
       <div className='fixed left-0 top-0 z-10 hidden w-full flex-col bg-white p-2 md:flex'>
         <div className={`flex justify-center pb-2`}>
-          <img className='h-12 w-12' src='logo.png' alt='icon' />
-          <img className='h-12 w-12' src='logo.png' alt='icon' />
-          <img className='h-12 w-12' src='logo.png' alt='icon' />
-          <img className='h-12 w-12' src='logo.png' alt='icon' />
-          <img className='h-12 w-12' src='logo.png' alt='icon' />
+          <img
+            className='h-12 w-12 object-contain'
+            src='/logos/isj.png'
+            alt='icon'
+          />
+          <img
+            className='h-12 w-12 object-contain'
+            src='/logos/me.png'
+            alt='icon'
+          />
+          <img
+            className='h-12 w-12 object-contain'
+            src='/logos/primaria.png'
+            alt='icon'
+          />
+          <img
+            className='h-12 w-12 object-contain'
+            src='/logos/consiliu.png'
+            alt='icon'
+          />
+          <img
+            className='h-12 w-12 object-contain'
+            src='/logos/saguna.jpg'
+            alt='icon'
+          />
         </div>
         <div className='flex items-center justify-between pr-4'>
           <img className='h-12 w-12' src='logo.png' alt='icon' />
-          <div className='flex flex-col'>
-            <label className={`'text-[1rem] text-center font-bold text-black`}>
-              OLIMPIADA DE ISTORIE - ETAPA NATIONALA
-            </label>
-            <label className={`text-center text-[1rem] font-bold text-black`}>
-              EDITIA XLVII, 2024
-            </label>
-          </div>
+          {/*<div className='flex flex-col'>*/}
+          {/*  <label className={`'text-[1rem] text-center font-bold text-black`}>*/}
+          {/*    OLIMPIADA DE ISTORIE - ETAPA NATIONALA*/}
+          {/*  </label>*/}
+          {/*  <label className={`text-center text-[1rem] font-bold text-black`}>*/}
+          {/*    EDITIA XLVII, 2024*/}
+          {/*  </label>*/}
+          {/*</div>*/}
           <i
             onClick={() => {
               setOpen(!open);
