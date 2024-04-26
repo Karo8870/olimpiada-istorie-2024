a1ddcb40110cf088e626e86e416b4b96549e335d
alex <simedrea.alex@gmail.com>
2024-04-26 15:05:26 +0300

    added curse-speciale

---
+++ src/app/informatii/curse-speciale/page.tsx
@@ -0,0 +1,19 @@
+export default function Regulament() {
+  return (
+    <main className='flex flex-col gap-8 px-16 pb-16 pt-36 sm:px-4'>
+      {/*<div className='relative flex w-full flex-col items-center bg-cover'>*/}
+      {/*  <img*/}
+      {/*    className='absolute z-[0] h-full w-full rounded-[1.25rem] object-cover [filter:brightness(65%)]'*/}
+      {/*    src='/brasov4.jpeg'*/}
+      {/*    alt='Brasov'*/}
+      {/*  />*/}
+      {/*  <div className='mb-48 mt-48 flex w-[80%] max-w-[60rem] flex-col items-center'>*/}
+      <h1 className='relative z-10 text-[3rem] font-bold text-center md:text-3xl'>
+        Program curse speciale RATBV
+      </h1>
+      <embed src="https://firebasestorage.googleapis.com/v0/b/olimpiada-istorie-2024.appspot.com/o/PROGRAM%20CURSE%20SPECIALE%20RAT.pdf?alt=media&token=6f333662-9c8f-4668-826f-380236b5ccaa" type="application/pdf" height="1000" />
+      {/*</div>*/}
+      {/*</div>*/}
+    </main>
+  );
+}
